78e7d417cbd75c24f5c419659bbafe93b3d5b91e
vermilion10 <180294769+vermilion10@users.noreply.github.com>
2025-08-09 11:44:04 +0700

    update wuwa past event data

--- src/data/wuwaEvents.ts
+++ src/data/wuwaEvents.ts
@@ -7,7 +7,7 @@ export const wuwaEvents: GameEvent[] = [
     endTime: "2025-08-28T03:59:59Z",
     image: "https://scontent.fbdo2-1.fna.fbcdn.net/v/t39.30808-6/523902649_756071523645743_6966318952608826202_n.png?_nc_cat=101&ccb=1-7&_nc_sid=cc71e4&_nc_eui2=AeGQWgMoNBeU8fxhMmBv2Ldt9MFLxxYj5LL0wUvHFiPksucEDxRe2V1FyQV19D8SLi5Yj8qiTceJ4Jwc9FFLpJjL&_nc_ohc=U7xcsSXzOtAQ7kNvwGdvohZ&_nc_oc=Adm3YuuuQkHObU4WwufW2U3cbFFMl_CN6BJmhczFs-Y13fPRxVVgT4CO1lmOiHE0lns&_nc_zt=23&_nc_ht=scontent.fbdo2-1.fna&_nc_gid=fbTghDzkB8jvc32rv1omcQ&oh=00_AfWU6Y3DkqZCUEfTy9wdTelZMBUAq5Db2TBB5bSW-3eMmw&oe=68990C47",
     link: "https://wutheringwaves.kurogames.com/en/main/news/detail/2966",
-    description: "New story chapter, New resonator: Phrolova. Click for more details."
+    description: "Version 2.5 update. Click for more details."
   },
   {
     name: "Lightly We Toss the Crown",
@@ -15,7 +15,7 @@ export const wuwaEvents: GameEvent[] = [
     endTime: "2025-07-24T03:59:59Z",
     image: "https://scontent.fbdo2-1.fna.fbcdn.net/v/t39.30808-6/506627737_723904416862454_482212817227919553_n.png?stp=dst-jpg_tt6&_nc_cat=111&ccb=1-7&_nc_sid=86c6b0&_nc_eui2=AeGNxTl-wrIzy967wdJcf46oIJU7FF9QtOwglTsUX1C07APyaLyD8DnyLqaS7u7tm2mz2joExUAuWLi6lmZRQRmo&_nc_ohc=qfje6qNlk-UQ7kNvwGLuy8K&_nc_oc=AdnOn5JXmUimdvn3J1AAAwopHZdPBfOu4HFW_ryQnbdb51F8zmhYr-4laKrZeQ1xTvI&_nc_zt=23&_nc_ht=scontent.fbdo2-1.fna&_nc_gid=9mjkH0mN_aYbyc0nGdhXRQ&oh=00_AfXknhJ0rebOoBziNfownKlZtF4cnp6rWT7qqxxYYcQpMg&oe=6898EF75",
     link: "https://wutheringwaves.kurogames.com/en/main/news/detail/2801",
-    description: "New story chapter, new resonator: Carthethyia and Lupa. Click for more details."
+    description: "Version 2.4 update. Click for more details."
   },
   {
     name: "Fiery Arpeggio of Summer Reunion",
@@ -23,6 +23,70 @@ export const wuwaEvents: GameEvent[] = [
     endTime: "2025-06-12T03:59:59Z",
     image: "https://hw-media-cdn-mingchao.kurogame.com/object/1745769600000/pgtz2r6s6xtwfhogej-1745850723521.png",
     link: "https://wutheringwaves.kurogames.com/en/main/news/detail/2561",
-    description: "New story chapter, new resonator: Zani and Ciaccona. Click for more details."
+    description: "Version 2.3 update. Click for more details."
+  },
+  {
+    name: "Tangled Truth in Inverted Tower",
+    startTime: "2025-03-27T11:00:00Z",
+    endTime: "2025-04-29T03:59:59Z",
+    image: "https://hw-media-cdn-mingchao.kurogame.com/object/1742918400000/wtgbqai6v0890mkj4v-1742998535116.jpg",
+    link: "https://wutheringwaves.kurogames.com/en/main/news/detail/2381",
+    description: "Version 2.2 update. Click for more details."
+  },
+  {
+    name: "Waves Sing, and the Cerulean Bird Calls",
+    startTime: "2025-02-13T11:00:00Z",
+    endTime: "2025-03-27T03:59:59Z",
+    image: "https://scontent.fbdo2-1.fna.fbcdn.net/v/t39.30808-6/477662633_635728295680067_4855656662018065358_n.png?stp=dst-jpg_tt6&_nc_cat=103&ccb=1-7&_nc_sid=86c6b0&_nc_ohc=U15ugylSgzkQ7kNvwH6diOB&_nc_oc=AdlJIpyGKduKQEbClBNTXKZku1EOI69kdYQl0DiDFu8aIexRPDWHRuvxMnPwQ8ZoFHE&_nc_zt=23&_nc_ht=scontent.fbdo2-1.fna&_nc_gid=lkOkf3dVlRAKvb9OwHCRKQ&oh=00_AfUtueDXaiBTXlS3KpmbL4HTkZK06DFmAw2X2gh3jE8Cng&oe=689CB8B8",
+    link: "https://wutheringwaves.kurogames.com/en/main/news/detail/2154",
+    description: "Version 2.1 update. Click for more details."
+  },
+  {
+    name: "All Silent Souls Can Sing",
+    startTime: "2025-01-02T11:00:00Z",
+    endTime: "2025-02-13T03:59:59Z",
+    image: "https://scontent.fbdo2-1.fna.fbcdn.net/v/t39.30808-6/472217030_608077838442310_222401296008174271_n.png?_nc_cat=102&ccb=1-7&_nc_sid=86c6b0&_nc_ohc=UBFR78WOBE0Q7kNvwFi9E8C&_nc_oc=AdnkEXivvZ3V7oFkgBIx2yX9mRHH5nVy75wfr8TZTDyXv1INGRyoeSzWgHDdtqLRLrs&_nc_zt=23&_nc_ht=scontent.fbdo2-1.fna&_nc_gid=-AMMmTBwPrIjUJx2ctd1NA&oh=00_AfVKgSGpg9V0mRhhEGralhf_CtYnT4J7kARUgObIOuyE5g&oe=689C9CA3",
+    link: "https://wutheringwaves.kurogames.com/en/main/news/detail/1912",
+    description: "Version 2.0 update. Click for more details."
+  },
+  {
+    name: "When the Night Knocks",
+    startTime: "2024-11-14T11:00:00Z",
+    endTime: "2025-01-02T03:59:59Z",
+    image: "https://scontent.fbdo2-1.fna.fbcdn.net/v/t39.30808-6/466854233_574529921797102_6533239428635172754_n.png?_nc_cat=105&ccb=1-7&_nc_sid=86c6b0&_nc_ohc=vOPoAuXnuHgQ7kNvwEuY_ce&_nc_oc=AdmzxhILPStlksdEaMfzBNtV7xWAHsw7Rms4RqEvnl2GNl1hE_3yhu289dOZ32KjaW0&_nc_zt=23&_nc_ht=scontent.fbdo2-1.fna&_nc_gid=Gq2DQ2NGhRYTr0gKw7cNAQ&oh=00_AfW6S0izMqOxcwELe5OLIHBvkPrhm_ODg2EwL9853bOlMA&oe=689CA8B8",
+    link: "https://wutheringwaves.kurogames.com/en/main/news/detail/1647",
+    description: "Version 1.4 update. Click for more details."
+  },
+  {
+    name: "To the Shore's End",
+    startTime: "2024-09-29T11:00:00Z",
+    endTime: "2024-11-14T03:59:59Z",
+    image: "https://hw-media-cdn-mingchao.kurogame.com/object/1727539200000/tvmzq5afr0vqyom3lh-1727577702090.jpg",
+    link: "https://wutheringwaves.kurogames.com/en/main/news/detail/1454",
+    description: "Version 1.3 update. Click for more details."
+  },
+  {
+    name: "In the Turquoise Moonglow",
+    startTime: "2024-08-15T13:00:00Z",
+    endTime: "2024-09-29T03:59:59Z",
+    image: "https://hw-media-cdn-mingchao.kurogame.com/object/1723564800000/a7lm6t5wyzw8g8fwbk-1723639075036.jpg",
+    link: "https://wutheringwaves.kurogames.com/en/main/news/detail/1190",
+    description: "Version 1.2 update. Click for more details."
+  },
+  {
+    name: "Thaw of Eons",
+    startTime: "2024-06-28T13:00:00Z",
+    endTime: "2024-08-15T05:59:59Z",
+    image: "https://hw-media-cdn-mingchao.kurogame.com/object/1719504000000/riv0ofvthvam93e8qk-1719549764892.jpg",
+    link: "https://wutheringwaves.kurogames.com/en/main/news/detail/996",
+    description: "Version 1.1 update. Click for more details."
+  },
+    {
+    name: "Waking of a World",
+    startTime: "2024-05-23T10:00:00Z",
+    endTime: "2024-06-28T05:59:59Z",
+    image: "https://pbs.twimg.com/media/GJ0m-ReawAAeHu6?format=jpg&name=large",
+    link: "https://x.com/Wuthering_Waves/status/1773671444767002757",
+    description: "Version 1.0 release. Click for more details."
   },
 ];
